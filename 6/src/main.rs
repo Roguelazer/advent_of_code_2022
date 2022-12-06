7b993b449c2e23890e060de9fe60d5ea167d3621
James Brown <roguelazer@roguelazer.com>
2022-12-06 09:05:20 -0800

    redo day 6 to stream input

--- 6/src/main.rs
+++ 6/src/main.rs
@@ -24,30 +24,65 @@ fn is_unique_bytes(s: &[u8], set: &mut bit_set::BitSet) -> bool {
     true
 }
 
-fn main() -> anyhow::Result<()> {
+struct RingyBuf<T: Default + Copy, const N: usize> {
+    storage: [T; N],
+    full: bool,
+    i: usize,
+}
+
+impl<T: Default + Copy, const N: usize> RingyBuf<T, N> {
+    fn new() -> Self {
+        Self {
+            storage: [T::default(); N],
+            full: false,
+            i: 0,
+        }
+    }
+
+    fn push(&mut self, value: T) {
+        self.storage[self.i] = value;
+        self.i = (self.i + 1) % N;
+        if self.i == 0 {
+            self.full = true;
+        }
+    }
+
+    fn is_full(&self) -> bool {
+        self.full
+    }
+
+    fn bytes(&self) -> &[T] {
+        if self.full {
+            &self.storage
+        } else {
+            &self.storage[0..self.i]
+        }
+    }
+}
+
+fn run<const N: usize, R: Read>(io: R) -> Option<usize> {
+    let mut r = RingyBuf::<u8, N>::new();
+    let mut s = bit_set::BitSet::with_capacity(256);
+    for (i, b) in io.bytes().filter_map(Result::ok).enumerate() {
+        r.push(b);
+        if r.is_full() {
+            s.clear();
+            if is_unique_bytes(r.bytes(), &mut s) {
+                return Some(i + 1);
+            }
+        }
+    }
+    None
+}
+
+fn main() {
     let args = Args::parse();
     let stdin = std::io::stdin();
-    let mut handle = stdin.lock();
-    let sequence_size = match args.mode {
-        Mode::Part1 => 4,
-        Mode::Part2 => 14,
-    };
-    let mut input = Vec::new();
-    handle.read_to_end(&mut input)?;
-    let mut s = bit_set::BitSet::with_capacity(256);
-    let res = input
-        .as_slice()
-        .windows(sequence_size)
-        .enumerate()
-        .find_map(|(i, window)| {
-            s.clear();
-            if is_unique_bytes(window, &mut s) {
-                Some(i + sequence_size)
-            } else {
-                None
-            }
-        })
-        .ok_or_else(|| anyhow::anyhow!("no marker found!"))?;
-    println!("{:?}", res);
-    Ok(())
+    let handle = stdin.lock();
+    if let Some(found) = match args.mode {
+        Mode::Part1 => run::<4, _>(handle),
+        Mode::Part2 => run::<14, _>(handle),
+    } {
+        println!("{:?}", found);
+    }
 }
