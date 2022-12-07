81c962b15268f71186f767d4c29a4207ee33b9f1
James Brown <roguelazer@roguelazer.com>
2022-12-07 09:41:43 -0800

    structure more like a real filesystem

--- 7/src/main.rs
+++ 7/src/main.rs
@@ -1,7 +1,8 @@
-use std::collections::BTreeMap;
 use std::io::BufRead;
 
 use clap::{Parser, ValueEnum};
+
+mod fs;
 
 #[derive(ValueEnum, Debug, PartialEq, Eq, Clone, Copy)]
 enum Mode {
@@ -17,192 +18,77 @@ struct Args {
 }
 
 #[derive(Debug, PartialEq, Eq)]
-struct File {
-    size: usize,
-}
-
-#[derive(Debug, PartialEq, Eq)]
-struct Directory {
-    files: BTreeMap<String, File>,
-    children: BTreeMap<String, Directory>,
-    size: usize,
-}
-
-impl Directory {
-    fn new() -> Self {
-        Self {
-            files: BTreeMap::new(),
-            children: BTreeMap::new(),
-            size: 0,
-        }
-    }
-
-    fn cache_directory_sizes(&mut self) {
-        self.size = self.files.values().map(|f| f.size).sum();
-        if self.children.len() > 0 {
-            for child in self.children.values_mut() {
-                child.cache_directory_sizes();
-            }
-            self.size += self.children.values().map(|f| f.size).sum::<usize>();
-        }
-    }
-
-    fn walk<F, T>(&self, start_path: Path, f: &mut F)
-    where
-        F: FnMut(Path, &Directory) -> T,
-    {
-        f(start_path.clone(), &self);
-        for (name, child) in self.children.iter() {
-            let path = start_path.with(name);
-            child.walk(path, f);
-        }
-    }
-
-    fn get_mut(&mut self, components: &Path) -> anyhow::Result<&mut Directory> {
-        let mut top: &mut Directory = self;
-        for component in components.components.iter() {
-            top = top
-                .children
-                .get_mut(component)
-                .ok_or_else(|| anyhow::anyhow!("invalid path!"))?;
-        }
-        Ok(top)
-    }
-
-    fn add_file(&mut self, path: &str, size: usize) {
-        self.files.insert(path.to_owned(), File { size });
-    }
-
-    fn add_directory(&mut self, path: &str) {
-        self.children.insert(path.to_owned(), Directory::new());
-    }
-}
-
-#[derive(Debug)]
-struct Filesystem {
-    root: Directory,
-}
-
-#[derive(Debug, PartialEq, Eq)]
 enum Command {
     Ls,
 }
 
-#[derive(Debug, Clone)]
-struct Path {
-    components: Vec<String>,
-}
-
-impl Path {
-    fn new() -> Self {
-        Self {
-            components: Vec::new(),
-        }
-    }
-
-    fn pop_up(&mut self) {
-        self.components.pop();
-    }
-
-    fn push<S: Into<String>>(&mut self, component: S) {
-        self.components.push(component.into());
-    }
-
-    fn with<S: Into<String>>(&self, component: S) -> Self {
-        let mut new = self.clone();
-        new.push(component);
-        new
-    }
-}
-
-impl std::fmt::Display for Path {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "/{}", self.components.join("/"))
-    }
-}
-
-impl Filesystem {
-    fn parse<R: BufRead>(reader: R) -> anyhow::Result<Self> {
-        let mut root = Directory::new();
-        let mut cwd = Path::new();
-        let mut command = None;
-        for line in reader.lines() {
-            let line = line?;
-            if line.chars().next() == Some('$') {
-                let mut parts = line.split(' ');
-                let command_run = parts
-                    .nth(1)
-                    .ok_or_else(|| anyhow::anyhow!("invalid command line"))?;
-                match command_run {
-                    "cd" => {
-                        let target_path = parts
-                            .next()
-                            .ok_or_else(|| anyhow::anyhow!("missing path for cd"))?;
-                        if target_path == "/" {
-                            cwd = Path::new();
-                        } else if target_path == ".." {
-                            cwd.pop_up();
-                        } else {
-                            cwd.push(target_path);
-                        }
+fn populate_filesystem_from_commands<R: BufRead>(reader: R) -> anyhow::Result<fs::Filesystem> {
+    let mut fs = fs::Filesystem::new();
+    let mut cwd = fs.get_root_path();
+    let mut command = None;
+    for line in reader.lines() {
+        let line = line?;
+        if line.starts_with('$') {
+            let mut parts = line.split(' ');
+            let command_run = parts
+                .nth(1)
+                .ok_or_else(|| anyhow::anyhow!("invalid command line"))?;
+            match command_run {
+                "cd" => {
+                    let target_path = parts
+                        .next()
+                        .ok_or_else(|| anyhow::anyhow!("missing path for cd"))?;
+                    if target_path == "/" {
+                        cwd = fs.get_root_path();
+                    } else if target_path == ".." {
+                        cwd.pop_up();
+                    } else {
+                        cwd.cd(target_path, &fs)?;
                     }
-                    "ls" => command = Some(Command::Ls),
-                    c => anyhow::bail!("unhandled command {}", c),
                 }
-                // command
-            } else {
-                match command {
-                    Some(Command::Ls) => {
-                        if let Some((stat, label)) = line.split_once(' ') {
-                            if stat == "dir" {
-                                root.get_mut(&cwd)?.add_directory(label);
-                            } else {
-                                let size = stat.parse()?;
-                                root.get_mut(&cwd)?.add_file(label, size);
-                            }
+                "ls" => command = Some(Command::Ls),
+                c => anyhow::bail!("unhandled command {}", c),
+            }
+        } else {
+            match command {
+                Some(Command::Ls) => {
+                    if let Some((stat, label)) = line.split_once(' ') {
+                        if stat == "dir" {
+                            fs.add_directory(&cwd, label)?;
                         } else {
-                            anyhow::bail!("invalid output line {:?}", line);
+                            let size = stat.parse()?;
+                            fs.add_file(&cwd, label, size)?;
                         }
+                    } else {
+                        anyhow::bail!("invalid output line {:?}", line);
                     }
-                    None => anyhow::bail!("output without a running command"),
                 }
+                None => anyhow::bail!("output without a running command"),
             }
         }
-        Ok(Self { root })
     }
-
-    fn cache_directory_sizes(&mut self) {
-        self.root.cache_directory_sizes();
-    }
-
-    fn walk<F, T>(&self, mut f: F)
-    where
-        F: FnMut(Path, &Directory) -> T,
-    {
-        let path = Path::new();
-        self.root.walk(path, &mut f);
-    }
+    Ok(fs)
 }
 
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let stdin = std::io::stdin();
-    let handle = stdin.lock();
-    let mut fs = Filesystem::parse(handle)?;
-    let mut total_size = 0;
-    fs.cache_directory_sizes();
+    let mut handle = stdin.lock();
+    let mut fs = populate_filesystem_from_commands(&mut handle)?;
+    fs.cache_directory_sizes()?;
     match args.mode {
         Mode::Part1 => {
-            fs.walk(|_path, dir| {
-                if dir.size < 100000 {
-                    total_size += dir.size;
+            let mut total_size = 0;
+            fs.walk(|_, item| {
+                if item.is_dir() && item.size() < 100000 {
+                    total_size += item.size();
                 }
             });
             println!("total_size = {}", total_size);
         }
         Mode::Part2 => {
             let mut best_candidate = None;
-            let root_size = fs.root.size;
+            let root_size = fs.get_root_dir().size;
             if root_size > 70000000 {
                 anyhow::bail!("FS is too big!");
             }
@@ -211,11 +97,13 @@ fn main() -> anyhow::Result<()> {
                 anyhow::bail!("FS already has 30000000B free");
             }
             let needed = 30000000 - free;
-            fs.walk(|path, dir| {
-                if dir.size > needed {
+            fs.walk(|path, item| {
+                if item.is_dir() && item.size() > needed {
                     match best_candidate {
-                        None => best_candidate = Some((path, dir.size)),
-                        Some((_, c)) if c > dir.size => best_candidate = Some((path, dir.size)),
+                        None => best_candidate = Some((path, item.size())),
+                        Some((_, c)) if c > item.size() => {
+                            best_candidate = Some((path, item.size()))
+                        }
                         _ => {}
                     }
                 }
