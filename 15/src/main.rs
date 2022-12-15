6f4308e5c3e2cb7fffcea16a65a33e9a3361e922
James Brown <roguelazer@roguelazer.com>
2022-12-14 23:16:25 -0800

    clean up part 2

--- 15/src/main.rs
+++ 15/src/main.rs
@@ -1,7 +1,6 @@
 use std::ops::RangeInclusive;
 
 use clap::{Parser, ValueEnum};
-use itertools::Itertools;
 use nom::{
     bytes::complete::tag,
     combinator::map,
@@ -30,14 +29,14 @@ struct Args {
 }
 
 #[derive(Debug)]
-struct SensorLine {
+struct Sensor {
     me: Point,
     #[allow(dead_code)]
     neighbor: Point,
     radius: usize,
 }
 
-impl SensorLine {
+impl Sensor {
     fn new(me: Point, neighbor: Point) -> Self {
         let radius = me.manhattan_distance_to(neighbor);
         Self {
@@ -47,28 +46,28 @@ impl SensorLine {
         }
     }
 
-    fn occludes(&self, point: Point) -> bool {
+    pub fn occludes(&self, point: Point) -> bool {
         self.me.manhattan_distance_to(point) <= self.radius
     }
 
-    fn projected_to_x(&self, x: i64) -> Option<RangeInclusive<i64>> {
-        if (self.me.x - x).abs() > self.radius as i64 {
+    fn projected_to(&self, my: i64, their: i64, center: i64) -> Option<RangeInclusive<i64>> {
+        let distance = (my - their).abs();
+        if distance > self.radius as i64 {
             None
         } else {
-            let distance = (self.me.x - x).abs();
             let radius_at_distance = self.radius as i64 - distance;
-            Some((self.me.y - radius_at_distance)..=(self.me.y + radius_at_distance))
+            Some((center - radius_at_distance)..=(center + radius_at_distance))
         }
     }
 
-    fn projected_to_y(&self, y: i64) -> Option<RangeInclusive<i64>> {
-        if (self.me.y - y).abs() > self.radius as i64 {
-            None
-        } else {
-            let distance = (self.me.y - y).abs();
-            let radius_at_distance = self.radius as i64 - distance;
-            Some((self.me.x - radius_at_distance)..=(self.me.x + radius_at_distance))
-        }
+    /// Find the number of points covered on a line at going from (x, -inf) to (x, inf)
+    pub fn projected_to_x(&self, x: i64) -> Option<RangeInclusive<i64>> {
+        self.projected_to(self.me.x, x, self.me.y)
+    }
+
+    /// Find the number of points covered on a line at going from (-inf, y) to (inf, y)
+    pub fn projected_to_y(&self, y: i64) -> Option<RangeInclusive<i64>> {
+        self.projected_to(self.me.y, y, self.me.x)
     }
 }
 
@@ -83,18 +82,18 @@ fn parse_coordinate(s: &str) -> IResult<&str, Point> {
     )(s)
 }
 
-fn parse_sensor_line(s: &str) -> IResult<&str, SensorLine> {
+fn parse_sensor_line(s: &str) -> IResult<&str, Sensor> {
     map(
         separated_pair(
             preceded(tag("Sensor at "), parse_coordinate),
             tag(": closest beacon is at "),
             parse_coordinate,
         ),
-        |(me, neighbor)| SensorLine::new(me, neighbor),
+        |(me, neighbor)| Sensor::new(me, neighbor),
     )(s)
 }
 
-fn parse_sensor_lines(s: &str) -> anyhow::Result<Vec<SensorLine>> {
+fn parse_sensor_lines(s: &str) -> anyhow::Result<Vec<Sensor>> {
     let (remaining, lines) = separated_list1(tag("\n"), parse_sensor_line)(s)
         .map_err(|e| anyhow::anyhow!("invalid line {:?}", e))?;
 
@@ -102,6 +101,28 @@ fn parse_sensor_lines(s: &str) -> anyhow::Result<Vec<SensorLine>> {
         anyhow::bail!("unhandled input {:?}", remaining);
     }
     Ok(lines)
+}
+
+/// Merge together an iterator of RangeInclusive objects and find whether there are any gaps
+/// between min..=max
+fn has_gap_in_ranges<I: Iterator<Item = RangeInclusive<i64>>>(i: I, min: i64, max: i64) -> bool {
+    let mut current = None;
+    for range in i {
+        match current {
+            None => current = Some(range),
+            Some(c) if *range.start() <= *c.end() + 1 => {
+                current = Some(*c.start()..=std::cmp::max(*range.end(), *c.end()))
+            }
+            Some(c) => {
+                if *c.end() < min || *c.start() > max {
+                    current = Some(range)
+                } else {
+                    return true;
+                }
+            }
+        }
+    }
+    false
 }
 
 fn main() -> anyhow::Result<()> {
@@ -118,6 +139,8 @@ fn main() -> anyhow::Result<()> {
         .init();
     let stdin = std::io::stdin();
     let input = std::io::read_to_string(stdin)?;
+    let start = std::time::Instant::now();
+    log::debug!("parsing input");
     let lines = parse_sensor_lines(&input)?;
     if args.mode == Mode::Part1 {
         let possible_x_coordinates = lines
@@ -167,53 +190,32 @@ fn main() -> anyhow::Result<()> {
     } else {
         let min = 0;
         let max = args.target_line;
+        let mut buf = Vec::with_capacity(lines.len());
+        log::debug!("scanning for potential x coordinates");
         let non_covered_x = (min..=max)
             .find(|x| {
-                let mut current = None;
-                for range in lines
-                    .iter()
-                    .filter_map(|sensor| sensor.projected_to_x(*x))
-                    .sorted_by_key(|r| *r.start())
-                {
-                    match current {
-                        None => current = Some(range),
-                        Some(c) if *range.start() <= *c.end() + 1 => {
-                            current = Some(*c.start()..=std::cmp::max(*range.end(), *c.end()))
-                        }
-                        _ => {
-                            return true;
-                        }
-                    }
-                }
-                false
+                buf.clear();
+                buf.extend(lines.iter().filter_map(|sensor| sensor.projected_to_x(*x)));
+                buf.sort_by_key(|r| *r.start());
+                has_gap_in_ranges(buf.drain(0..), min, max)
             })
             .unwrap();
+        log::debug!("scanning for potential y coordinates");
         let non_covered_y = (min..=max)
             .find(|y| {
-                let mut current = None;
-                for range in lines
-                    .iter()
-                    .filter_map(|sensor| sensor.projected_to_y(*y))
-                    .sorted_by_key(|r| *r.start())
-                {
-                    match current {
-                        None => current = Some(range),
-                        Some(c) if *range.start() <= *c.end() + 1 => {
-                            current = Some(*c.start()..=std::cmp::max(*range.end(), *c.end()))
-                        }
-                        _ => {
-                            return true;
-                        }
-                    }
-                }
-                false
+                buf.clear();
+                buf.extend(lines.iter().filter_map(|sensor| sensor.projected_to_y(*y)));
+                buf.sort_by_key(|r| *r.start());
+                has_gap_in_ranges(buf.drain(0..), min, max)
             })
             .unwrap();
         let point = Point::new(non_covered_x, non_covered_y);
+        log::debug!("double-checking for occlusions");
         if lines.iter().any(|s| s.occludes(point)) {
             panic!("uh oh! occlusion!")
         }
-        println!("{}", point.x * 4000000 + point.y)
+        log::info!("Frequency {} at {}", point.x * 4000000 + point.y, point);
+        log::debug!("succeeded in {:?}", start.elapsed());
     }
     Ok(())
 }
